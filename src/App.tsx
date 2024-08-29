e387c313171b935287b7a97195e27a059d0c6a55
Agammoudi <148436108+Agammoudi@users.noreply.github.com>
2024-08-29 16:17:22 +0200

    Etape 2
    Ajout liste des pokémon

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, fileTray, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -44,6 +44,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import PokemonList from './pages/PokemonsList';
 
 setupIonicReact();
 
@@ -64,6 +65,7 @@ const App: React.FC = () => (
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
+          <Route path="/pokemon-list" component={PokemonList} />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
@@ -77,6 +79,10 @@ const App: React.FC = () => (
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
             <IonLabel>Tab 3</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="pokemon-list" href="/pokemon-list">
+            <IonIcon aria-hidden="true" icon={fileTray} />
+            <IonLabel>Pokemon</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
---
+++ src/pages/PokemonsList.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonChip, IonLabel } from '@ionic/react';
+import axios from 'axios';
+
+interface Pokemon {
+    pokedex_id: number;
+	generation: number;
+	category: string;
+	name: {
+		fr: string;
+		en: string;
+		jp: string;
+	};
+	sprites: {
+		regular: string;
+		shiny: string;
+		gmax: string | null;
+	};
+	types: {
+		name: string;
+		image: string;
+	}[];
+	talents: {
+		name: string;
+		tc: boolean;
+	}[];
+	stats: {
+		hp: number;
+		atk: number;
+		def: number;
+		spe_atk: number;
+		spe_def: number;
+		vit: number;
+	};
+	resistances: {
+		name: string;
+		multiplier: number;
+	}[];
+	evolution: {
+		pre:
+			| {
+					pokedex_id: number;
+					name: string;
+					condition: string;
+			  }[]
+			| null;
+		next:
+			| {
+					pokedex_id: number;
+					name: string;
+					condition: string;
+			  }[]
+			| null;
+		mega:
+			| {
+					orbe: string;
+					sprites: {
+						regular: string;
+						shiny: string;
+					};
+			  }[]
+			| null;
+	};
+	height: string;
+	weight: string;
+	egg_groups: string[];
+	sexe: {
+		male: number;
+		female: number;
+	};
+	catch_rate: number;
+	level_100: number;
+	formes: any;
+}
+
+const PokemonList: React.FC = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    axios.get('https://tyradex.vercel.app/api/v1/pokemon')
+      .then(response => {
+        setPokemons(response.data);
+      })
+      .catch(error => {
+        console.error("Erreur lors de la récupération des données", error);
+      });
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Liste des Pokémon</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            {pokemons.map(pokemon => (
+              <IonCol size="6" key={pokemon.pokedex_id}>
+                <IonCard>
+                  <img src={pokemon.sprites.regular} alt={pokemon.name.fr} />
+                  <IonCardHeader>
+                    <IonCardTitle>{pokemon.name.fr}</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <p>Types:</p>
+                    {pokemon.types && pokemon.types.map((type, index) => (
+                      <IonChip key={index}>
+                        <IonLabel>{type.name}</IonLabel>
+                      </IonChip>
+                    ))}
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default PokemonList;
